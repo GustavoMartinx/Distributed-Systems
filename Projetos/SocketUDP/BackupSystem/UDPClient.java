d215ed813d775b6978973632e2884266d4a745e5
Gustavo Martins <gtavomartins893@gmail.com>
2024-04-24 15:48:15 -0300

    lab2: creating first packet to send when upload a file

--- Projetos/SocketUDP/BackupSystem/UDPClient.java
+++ Projetos/SocketUDP/BackupSystem/UDPClient.java
@@ -5,6 +5,8 @@ package BackupSystem;
  * Descrição: Envia uma mensagem em um datagrama e recebe a mesma mensagem do servidor
 **/
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Scanner;
 import java.io.*;
 
@@ -43,14 +45,7 @@ public class UDPClient {
                     continue;
                 }
 
-                byte[] m = command.getBytes(); // transforma a mensagem em bytes
-
-                /* cria um pacote datagrama */
-                DatagramPacket request
-                        = new DatagramPacket(m, m.length, serverAddr, serverPort);
-
-                /* envia o pacote */
-                dgramSocket.send(request);
+                uploadFile(command, serverAddr, serverPort, dgramSocket);
 
                 /* cria um buffer vazio para receber datagramas */
                 byte[] buffer = new byte[1000];
@@ -70,23 +65,60 @@ public class UDPClient {
             System.out.println("Socket: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("IO: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
         } // catch
     } // main
 
     static int handleCommand(String command) throws IOException {
         
         if (command.equals("EXIT")) {
-            return 0;
+            return 0;       // 0: command == "EXIT"
         } else {
             String filename = command;
             File fileInCurrentDir = new File(System.getProperty("user.dir") + "/" + filename);
             if (!fileInCurrentDir.exists()) {
                 System.out.println("Invalid command or file not found.");
-                return 1;
+                return 1;   // 1: command is invalid
             } else {
-                return 2;
+                return 2;   // 2: file exists
             }
         }
-        
     } // handleCommand
+
+    static void uploadFile(String command, InetAddress serverAddr, int serverPort, DatagramSocket dgramSocket) throws Exception {
+        
+        // Criando o primeiro pacote a ser enviado (FileName e FileSize):
+        
+        // File name: String (variável)
+        String fileName = command;
+        File file = new File(System.getProperty("user.dir") + "/" + fileName);
+        
+        // File size: Int (4 bytes)
+        long fileSize = file.length();
+        int fileSizeInt = (int) fileSize;
+        
+        // Inserindo esses dados no buffer
+        int sizeOfFileName = fileName.length();
+
+        ByteBuffer buffer = ByteBuffer.allocate(sizeOfFileName + 8); // tamanho do filename + 4 bytes
+        buffer.order(ByteOrder.BIG_ENDIAN);
+
+        byte[] fileNameBytes = fileName.getBytes();     // Transforma o nome do aquivo em bytes
+        buffer.put(fileNameBytes);                      // Insere o nome do aquivo no buffer
+        buffer.putInt(fileSizeInt);                        // Insere o tamanho do aquivo no buffer
+        byte[] payload = buffer.array();                // Converte o buffer em um array de bytes
+
+        // Cria um pacote datagrama com o buffer como payload
+        DatagramPacket request = new DatagramPacket(
+            payload,
+            payload.length,
+            serverAddr,
+            serverPort
+        );
+
+        // Envia o pacote
+        dgramSocket.send(request);
+    }
+
 } // class
